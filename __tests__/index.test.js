eb71a05bb4c7f58dfd347daca25449c1364e39a3
Mihael Stormrage <mihan.msn@hotmail.com>
2021-04-28 12:12:58 +0700

    Add --help test

---
+++ __tests__/index.test.js
@@ -0,0 +1,13 @@
+import outdent from 'outdent';
+import gendiff from '../src';
+
+test('gendiff --help', () => {
+  expect(gendiff.helpInformation()).toBe(outdent`
+    Usage: gendiff [options]
+
+    Compares two configuration files and shows a difference.
+
+    Options:
+      -V, --version  output the version number
+      -h, --help     output usage information`);
+});
