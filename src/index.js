c73acdf9e9e90bbbe916f7991566a241d12ac0c7
Mihael Stormrage <mihan.msn@hotmail.com>
2021-04-28 13:16:04 +0700

    Add arguments (pass test)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import { Command } from 'commander/esm.mjs';
 
 const program = new Command();
 
-program.name('gendiff').version('0.0.2')
+program.name('gendiff').version('0.0.3')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
   .helpOption('', 'output usage information')
   .option('-f, --format [type]', 'output format');
 
