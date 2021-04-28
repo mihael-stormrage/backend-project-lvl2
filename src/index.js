79731860b9f95a1db7b2ba144031a987ab3caa66
Mihael Stormrage <mihan.msn@hotmail.com>
2021-04-29 00:28:13 +0700

    Implement getData() (pass test)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { Command } from 'commander/esm.mjs';
+import fs from 'fs';
 
-export const getData = (path) => {};
+export const getData = (file) => JSON.parse(fs.readFileSync(file, 'utf8'));
 
 const genDiff = (data1, data2) => {
 
@@ -8,7 +9,7 @@ const genDiff = (data1, data2) => {
 
 const program = new Command();
 
-program.name('gendiff').version('0.0.4')
+program.name('gendiff').version('0.0.5')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .helpOption('', 'output usage information')
