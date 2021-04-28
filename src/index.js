f874d852df39e6f29bc9718701a99a49969a630a
Mihael Stormrage <mihan.msn@hotmail.com>
2021-04-29 03:34:55 +0700

    Implement genDiff() for flat objects (pass test)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import { Command } from 'commander/esm.mjs';
 import fs from 'fs';
+import _ from 'lodash';
 
 const getData = (file) => JSON.parse(fs.readFileSync(file, 'utf8'));
 
 const genDiff = (data1, data2) => {
-
+  if (_.isEqual(data1, data2)) return JSON.stringify(data1);
+  const indent = '  ';
+  const diff = _.mergeWith(_.cloneDeep(data1), data2, (oldVal, newVal) => ({ oldVal, newVal }));
+  const lines = _.sortBy(Object.entries(diff))
+    .map(([k, val]) => [k, typeof val !== 'object' ? { oldVal: val } : val])
+    .reduce((acc, [key, { oldVal, newVal }]) => {
+      if (oldVal === newVal) {
+        acc.push(`${indent.repeat(2)}${key}: ${oldVal}`);
+        return acc;
+      }
+      if (oldVal !== undefined) acc.push(`${indent}- ${key}: ${oldVal}`);
+      if (newVal !== undefined) acc.push(`${indent}+ ${key}: ${newVal}`);
+      return acc;
+    }, []);
+  return `{\n${lines.join('\n')}\n}`;
 };
 
 const program = new Command();
