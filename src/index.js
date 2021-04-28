f8b0182ad85c15b989354a75ce38a984144cc132
Mihael Stormrage <mihan.msn@hotmail.com>
2021-04-28 13:09:22 +0700

    Add --format option

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const program = new Command();
 
 program.name('gendiff').version('0.0.2')
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('', 'output usage information');
+  .helpOption('', 'output usage information')
+  .option('-f, --format [type]', 'output format');
 
 export default program;
