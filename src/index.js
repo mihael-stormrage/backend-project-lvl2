1a9ca7761424383fa90677ba326822e7d75db0b6
Mihael Stormrage <mihan.msn@hotmail.com>
2021-04-28 12:13:54 +0700

    Program init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { Command } from 'commander/esm.mjs';
+
+const program = new Command();
+program.version('0.0.1')
+  .description('Compares two configuration files and shows a difference.');
+
+export default program;
