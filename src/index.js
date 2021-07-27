91c2f012896686635cc33f070c24293c927b7e9c
Mihael Stormrage <mihan.msn@hotmail.com>
2021-07-28 06:32:11 +0700

    Implement AST for nested objects

--- src/index.js
+++ src/index.js
@@ -1,23 +1,46 @@
 import { Command } from 'commander/esm.mjs';
 import _ from 'lodash';
 import getData from './parsers.js';
+import stylish from './formatters.js';
 
-const genDiff = (data1, data2) => {
+const genDiff = (data1, data2, formatter = stylish) => {
   if (_.isEqual(data1, data2)) return 'No difference';
-  const indent = '  ';
-  const diff = _.mergeWith(_.cloneDeep(data1), data2, (oldVal, newVal) => ({ oldVal, newVal }));
-  const lines = _.sortBy(Object.entries(diff))
-    .map(([k, val]) => [k, typeof val !== 'object' ? { oldVal: val } : val])
-    .reduce((acc, [key, { oldVal, newVal }]) => {
-      if (oldVal === newVal) {
-        acc.push(`${indent.repeat(2)}${key}: ${oldVal}`);
-        return acc;
+
+  const makeDiffAst = (obj1, obj2) => {
+    const keys = [...new Set([...Object.keys(obj1), ...Object.keys(obj2)])];
+    return keys.flatMap((key) => {
+      const val1 = obj1[key];
+      const val2 = obj2[key];
+      const isNested = (v) => typeof v === 'object' && v !== null;
+
+      const processChanged = (val, name, type) => {
+        const result = [];
+        if (val !== undefined) {
+          const node = { type, name, nested: isNested(val) };
+          if (node.nested) node.children = _.flatMap(val, (v, k) => processChanged(v, k, 'unchanged'));
+          else node.value = val;
+          result.push(node);
+        }
+        return result;
+      };
+
+      switch (true) {
+        case isNested(val1) && isNested(val2):
+          return {
+            type: 'unchanged', name: key, children: makeDiffAst(val1, val2), nested: true,
+          };
+        case val1 === val2:
+          return {
+            type: 'unchanged', name: key, value: val1, nested: false,
+          };
+        default:
+          return [processChanged(val1, key, 'old'), processChanged(val2, key, 'new')].flat();
       }
-      if (oldVal !== undefined) acc.push(`${indent}- ${key}: ${oldVal}`);
-      if (newVal !== undefined) acc.push(`${indent}+ ${key}: ${newVal}`);
-      return acc;
-    }, []);
-  return `{\n${lines.join('\n')}\n}`;
+    });
+  };
+
+  const diff = makeDiffAst(data1, data2);
+  return formatter(diff);
 };
 
 const program = new Command();
